270dbfd5ecc874808101f6de1e516cb9ff04220f
spencerhardwick <spencerh@hey.com>
2021-02-20 22:01:24 +0000

    change the quotes in the arrays

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,10 +3,10 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ["1) Who was the first American Woman in Space? ", "2) True of false: 5000 meters = 5 kilometers. ", "3) (5+3)/2 * 10 =  ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
-let correctAnswer = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let question = ['1) Who was the first American Woman in Space? ', '2) True of false: 5000 meters = 5 kilometers. ', '3) (5+3)/2 * 10 =  ', "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", '5) What is the minimum crew size for the ISS? '];
+let correctAnswer = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
 let candidateAnswer = [];
 let questions = 5;
 let correctAnswers = 0;
@@ -15,7 +15,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Please enter your name: ")
+  candidateName = input.question('Please enter your name: ')
 }
 
 function askQuestion() {
@@ -24,7 +24,7 @@ function askQuestion() {
     candidateAnswer[i] = input.question(question[i]);
     console.log(`Your Answer: ${candidateAnswer[i]}`);
     console.log(`Correct Answer: ${correctAnswer[i]}`);
-    console.log("\n");
+    console.log('\n');
   }
 }
 
@@ -40,9 +40,9 @@ function gradeQuiz(candidateAnswers) {
   let gradePercentage = (correctAnswers) / (questions) * 100;
   let passOrFail;
     if (gradePercentage >= 80) {
-      passOrFail = "PASSED";
+      passOrFail = 'PASSED';
     } else {
-      passOrFail = "FAILED";
+      passOrFail = 'FAILED';
     }
   candidateAnswers = candidateAnswer.length;
   console.log(`>>> Overall Grade: ${gradePercentage}% (${correctAnswers} of ${candidateAnswers}) <<<`);
